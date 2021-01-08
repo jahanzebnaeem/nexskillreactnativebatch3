1663d79be4b10d0f72d920bcfe465a841b9d52df
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-01-08 17:54:15 +0500

    Lecture 2
    Started working on the header for the albums app.

---
+++ albums/src/components/Header.js
@@ -0,0 +1,11 @@
+// import helpful components
+import React from 'react';
+import {Text} from 'react-native';
+
+// make my component
+const Header = () => {
+  return <Text>Albums</Text>;
+};
+
+// export component to be used by other components
+export default Header;
