8b3a9b747f8aaeae02c77601fcc5022241faf856
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-01-30 22:24:41 +0500

    Lecture 8
    Creating Input component to have dynamic inputs.

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,29 +1,67 @@
 import React, {Component} from 'react';
-import {TextInput} from 'react-native';
-import {Card, CardSection, Button} from './common';
+import {Text} from 'react-native';
+import firebase from 'firebase';
+import {Card, CardSection, Button, Input} from './common';
 
 class LoginForm extends Component {
-  state = {text: ''};
+  state = {email: '', password: '', error: ''};
+
+  onButtonPress() {
+    const {email, password} = this.state;
+
+    this.setState({error: ''});
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({error: 'Authentication Failed.'});
+          });
+      });
+  }
 
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput
-            value={this.state.text}
-            onChangeText={(text) => this.setState({text})}
-            style={{width: 100, height: 20}}
+          <Input
+            placeholder="email@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={(email) => this.setState({email})}
           />
         </CardSection>
 
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={(password) => this.setState({password})}
+          />
+        </CardSection>
+
+        <Text style={styles.errorText}>{this.state.error}</Text>
 
         <CardSection>
-          <Button>Log In</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorText: {
+    fontSize: 20,
+    color: 'red',
+    textAlign: 'center',
+  },
+};
 
 export default LoginForm;
--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -1,8 +1,41 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, TextInput, Text} from 'react-native';
 
-const Input = () => {
-  return <View />;
+const Input = ({label, value, onChangeText, placeholder, secureTextEntry}) => {
+  const {containerStyle, textStyle, labelStyle} = styles;
+
+  return (
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
+      <TextInput
+        secureTextEntry={secureTextEntry}
+        autoCorrect={false}
+        style={textStyle}
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+      />
+    </View>
+  );
+};
+
+const styles = {
+  labelStyle: {
+    flex: 1,
+    fontSize: 16,
+    letterSpacing: 1,
+    lineHeight: 25,
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  textStyle: {
+    flex: 2,
+    fontSize: 16,
+  },
+  containerStyle: {
+    flex: 1,
+    flexDirection: 'row',
+  },
 };
 
 export {Input};
