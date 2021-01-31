79a85b24c02475a1755176168f4cd6787156c2f7
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-01-31 10:03:18 +0500

    Lecture 9
    Complete the auth app with the understanding of how to use component level states. Introduction to redux.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import firebase from 'firebase';
-import {Header} from './components/common';
+import {Header, Button, Card, CardSection, Spinner} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {loggedIn: null};
+
   componentDidMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDrYdikhvdfIvmA9tThcabxVEjtNTkRJyY',
@@ -15,13 +17,44 @@ class App extends Component {
       appId: '1:911944670632:web:be48504f7471e625d6d19d',
       measurementId: 'G-5HWZ8Y7QXH',
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <Card>
+            <CardSection>
+              <Spinner />
+            </CardSection>
+          </Card>
+        );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,27 +1,43 @@
 import React, {Component} from 'react';
 import {Text} from 'react-native';
 import firebase from 'firebase';
-import {Card, CardSection, Button, Input} from './common';
+import {Card, CardSection, Button, Input, Spinner} from './common';
 
 class LoginForm extends Component {
-  state = {email: '', password: '', error: ''};
+  state = {email: '', password: '', error: '', loading: false};
 
   onButtonPress() {
     const {email, password} = this.state;
 
-    this.setState({error: ''});
+    this.setState({error: '', loading: true});
 
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
+      .then(this.onLogInSuccess.bind(this))
       .catch(() => {
         firebase
           .auth()
           .createUserWithEmailAndPassword(email, password)
-          .catch(() => {
-            this.setState({error: 'Authentication Failed.'});
-          });
+          .then(this.onLogInSuccess.bind(this))
+          .catch(this.onLogInFail.bind(this));
       });
+  }
+
+  onLogInFail() {
+    this.setState({error: 'Authentication Failed.', loading: false});
+  }
+
+  onLogInSuccess() {
+    this.setState({email: '', password: '', error: '', loading: false});
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>;
   }
 
   render() {
@@ -48,9 +64,7 @@ class LoginForm extends Component {
 
         <Text style={styles.errorText}>{this.state.error}</Text>
 
-        <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>
-        </CardSection>
+        <CardSection>{this.renderButton()}</CardSection>
       </Card>
     );
   }
