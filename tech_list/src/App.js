22d3b4a3aa24fcea6ce1b8df6647ee202fbc9b86
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-02-07 13:13:47 +0500

    Lecture 10
    New Technical List project initilization and project setup.

---
+++ tech_list/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {Header} from './components/common';
+import reducers from './reducers';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech List" />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ tech_list/src/reducers/LibraryList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const LibraryList = () => {
+  return <View />;
+};
+
+export default LibraryList;
